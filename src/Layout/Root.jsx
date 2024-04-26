8a3701020bdeaaff996c7add26d5ea0463054431
gazi fayaz <gazifayaz.16694@gmail.com>
2024-04-26 14:24:44 +0600

    routes, firebase, AuthProvider setup and root layout setup

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+const Root = () => {
+	return (
+		<div className="font-poppins min-h-screen flex flex-col justify-between bg-[#DFF5FF]">
+			<Navbar></Navbar>
+			<div className="mx-6 md:mx-12 lg:mx-32">
+        <div>Root</div>
+				<Outlet></Outlet>
+				<ToastContainer />
+			</div>
+
+			<Footer></Footer>
+		</div>
+	);
+};
+
+export default Root;
